ef5a18371e7285930c556698f68c09ed00c594ba
balusena <balusena12@gmail.com>
2024-01-29 22:08:18 +0000

    roboshop jenkins files

--- vars/mainci.groovy
+++ vars/mainci.groovy
@@ -49,7 +49,7 @@ def call() {
                 }
                 sh 'rm -f Jenkinsfile'
                 sh 'echo ${TAG_NAME} >VERSION'
-                sh 'zip -r ${component}-${TA G_NAME}.zip *'
+                sh 'zip -r ${component}-${TAG_NAME}.zip *'
                 sh 'curl -v -u admin:admin123 --upload-file ${component}-${TAG_NAME}.zip http://172.31.22.180:8081/repository/${component}/${component}-${TAG_NAME}.zip'
             }
         }
