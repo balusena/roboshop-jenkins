78df7fe33fcaac2272a6c6806b9dbd27bc27cdfb
balusena <balusena12@gmail.com>
2024-01-29 21:59:11 +0000

    roboshop jenkins files

--- vars/mainci.groovy
+++ vars/mainci.groovy
@@ -1,11 +1,31 @@
 def call() {
     node('workstation') {
 
+        stage('Code Checkout') {
+
+            sh 'find . | grep "^./" |xargs rm -rf'
+
+            if(env.TAG_NAME ==~ ".*") {
+                env.gitbrname = "refs/tags/${env.TAG_NAME}"
+            } else {
+                env.gitbrname = "${env.BRANCH_NAME}"
+            }
+            checkout scm: [$class: 'GitSCM', userRemoteConfigs: [[url: "https://github.com/balusena/${env.component}"]], branches: [[name: gitbrname]]], poll: false
+
+        }
+
         if (env.cibuild == "java") {
             stage('Build') {
                 sh 'mvn package'
             }
         }
+
+//        if (env.cibuild == "nodejs") {
+//
+//            stage('Build') {
+//                sh 'npm install'
+//            }
+//        }
 
         stage('Unit tests') {
             echo 'unit tests'
@@ -21,10 +41,19 @@ def call() {
             echo 'Security Scans'
         }
 
-        if (env.TAG_NAME ==~ ".*") {
+        if(env.TAG_NAME ==~ ".*") {
             stage('Publish a Artifact') {
-                echo 'Publish a Artifact'
+                if (env.cibuild == "java") {
+                    sh 'mv target/${component}-1.0.jar ${component}.jar'
+                    sh 'rm -rf pom.xml src target'
+                }
+                sh 'rm -f Jenkinsfile'
+                sh 'echo ${TAG_NAME} >VERSION'
+                sh 'zip -r ${component}-${TA G_NAME}.zip *'
+                sh 'curl -v -u admin:admin123 --upload-file ${component}-${TAG_NAME}.zip http://172.31.22.180:8081/repository/${component}/${component}-${TAG_NAME}.zip'
             }
         }
     }
 }
+
+
